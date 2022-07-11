84d1b3a521acfac0ffc9261b478b6a084da60a16
Jahi Ghansam <jahighansam@gmail.com>
2022-07-11 16:24:22 -0400

    Completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...restArgs) {
 
+    let restArgsIndex;
+
+    // Outer loop runs through restArgs array
+    for(let j = 0; j < restArgs.length; j++){
+        // Assign current restArgs index to variable
+        restArgsIndex = restArgs[j];
+
+        // Inner loop compares restArgsIndex to "array" array and splices out any values that match
+        for(let i = 0; i < array.length; i++){
+            if(array[i] === restArgsIndex){
+                array.splice(i, 1);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
