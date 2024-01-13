8be38deabb6e35c2a736a9817d125509c48cc75b
Jahi Ghansam <jahighansam@gmail.com>
2024-01-13 15:30:13 -0500

    Created two functions which convert celcius to farenheit and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celcius = (temp - 32) * 5 / 9;
+  let roundedCelcius = Math.round(celcius * 10) / 10;
+  return roundedCelcius; 
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let farenheit = (temp *  9 / 5) + 32;
+  let roundedFarenheit = Math.round(farenheit * 10) / 10;
+  return roundedFarenheit;
 };
 
 // Do not edit below this line
